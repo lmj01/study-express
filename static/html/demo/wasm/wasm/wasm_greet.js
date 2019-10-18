4eddc8582362c05c3ee4e47d15a54111a0d0b982
lmj01 <lmjie_good@163.com>
2019-10-19 00:09:08 +0800

    update

--- static/html/demo/wasm/wasm/wasm_greet.js
+++ static/html/demo/wasm/wasm/wasm_greet.js
@@ -1,22 +1,13 @@
 
 let wasm;
 
-let WASM_VECTOR_LEN = 0;
-
-let cachedTextEncoder = new TextEncoder('utf-8');
-
-const encodeString = (typeof cachedTextEncoder.encodeInto === 'function'
-    ? function (arg, view) {
-    return cachedTextEncoder.encodeInto(arg, view);
+/**
+*/
+export function greet() {
+    wasm.greet();
 }
-    : function (arg, view) {
-    const buf = cachedTextEncoder.encode(arg);
-    view.set(buf);
-    return {
-        read: arg.length,
-        written: buf.length
-    };
-});
+
+let cachedTextDecoder = new TextDecoder('utf-8', { ignoreBOM: true, fatal: true });
 
 let cachegetUint8Memory = null;
 function getUint8Memory() {
@@ -25,44 +16,6 @@ function getUint8Memory() {
     }
     return cachegetUint8Memory;
 }
-
-function passStringToWasm(arg) {
-
-    let len = arg.length;
-    let ptr = wasm.__wbindgen_malloc(len);
-
-    const mem = getUint8Memory();
-
-    let offset = 0;
-
-    for (; offset < len; offset++) {
-        const code = arg.charCodeAt(offset);
-        if (code > 0x7F) break;
-        mem[ptr + offset] = code;
-    }
-
-    if (offset !== len) {
-        if (offset !== 0) {
-            arg = arg.slice(offset);
-        }
-        ptr = wasm.__wbindgen_realloc(ptr, len, len = offset + arg.length * 3);
-        const view = getUint8Memory().subarray(ptr + offset, ptr + len);
-        const ret = encodeString(arg, view);
-
-        offset += ret.written;
-    }
-
-    WASM_VECTOR_LEN = offset;
-    return ptr;
-}
-/**
-* @param {string} name
-*/
-export function greet(name) {
-    wasm.greet(passStringToWasm(name), WASM_VECTOR_LEN);
-}
-
-let cachedTextDecoder = new TextDecoder('utf-8', { ignoreBOM: true, fatal: true });
 
 function getStringFromWasm(ptr, len) {
     return cachedTextDecoder.decode(getUint8Memory().subarray(ptr, ptr + len));
@@ -75,7 +28,7 @@ function init(module) {
     let result;
     const imports = {};
     imports.wbg = {};
-    imports.wbg.__wbg_alert_c76ef920691a7581 = function(arg0, arg1) {
+    imports.wbg.__wbg_alert_da82f4cc3caa0034 = function(arg0, arg1) {
         alert(getStringFromWasm(arg0, arg1));
     };
 
